61c218f5257a3614146509c06e34cd84076df8a6
Petercodes <nuwaherezapeter34@gmail.com>
2025-05-27 14:22:10 +0000

    am creating a project first, did you add this code "// Import the functions you need from the SDKs you need import { initializeApp } from "firebase/app"; // TODO: Add SDKs for Firebase products that you want to use // https://firebase.google.com/docs/web/setup#available-libraries
    // Your web app's Firebase configuration
    const firebaseConfig = {
      apiKey: "AIzaSyDLc0DpKSMs1ESUHYaqXYU3NmuVY7biRaw",
      authDomain: "dairyconnect-7b93a.firebaseapp.com",
      projectId: "dairyconnect-7b93a",
      storageBucket: "dairyconnect-7b93a.firebasestorage.app",
      messagingSenderId: "715517548720",
      appId: "1:715517548720:web:610ce64f0ad61a69508375"
    };
    
    // Initialize Firebase
    const app = initializeApp(firebaseConfig);"

---
+++ src/lib/firebase.ts
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLc0DpKSMs1ESUHYaqXYU3NmuVY7biRaw",
+  authDomain: "dairyconnect-7b93a.firebaseapp.com",
+  projectId: "dairyconnect-7b93a",
+  storageBucket: "dairyconnect-7b93a.appspot.com", // Corrected from firebasestorage.app to appspot.com
+  messagingSenderId: "715517548720",
+  appId: "1:715517548720:web:610ce64f0ad61a69508375"
+};
+
+// Initialize Firebase
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+const auth = getAuth(app);
+const dbClient = getFirestore(app); // Renamed to dbClient to avoid conflict with db from firebaseAdmin
+
+export { app, auth, dbClient };
